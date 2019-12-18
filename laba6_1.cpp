3d2a7981af4b07135129039a1f8352b9abd9f81b
Максим <56740030+Maxim1720@users.noreply.github.com>
2019-12-18 14:08:12 +0400

    Add files via upload

---
+++ laba6_1.cpp
@@ -0,0 +1,32 @@
+﻿#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<malloc.h>
+
+int main()
+{
+	system("color C");
+	char string[100];
+	fgets(string, 100, stdin);
+	int new_size=0;
+	for (int i = 0;string[i] != '\0';i++) {
+		if (string[i] >='0' && string[i] <= '9')
+			new_size++;
+	}
+	new_size = strlen(string)+1 - new_size;
+	char* new_string;
+	new_string = (char*)calloc(new_size, sizeof(char));
+	if (new_string) {
+		int j = 0;
+		for (int i = 0;string[i] != '\0';i++) {
+			if (string[i]>='a' && string[i]<='z' || string[i]>='A' && string[i]<='Z') {
+				new_string[j] = string[i];
+				j++;
+			}
+		}
+		puts(new_string);
+	}
+	free(new_string);
+	return 0;
+}
+
---
+++ laba6_2.cpp
@@ -0,0 +1,17 @@
+﻿#include<string.h>
+#include<stdio.h>
+#include<string.h>
+
+
+int main()
+{
+	char String[100];
+	fgets(String, 100, stdin);
+	char ct[] = " ,.:;!?";
+	char* word = strtok(String, ct);
+	while (word != NULL) {
+		printf("%s\n", word);
+		word = strtok(NULL, ct);
+	}
+	return 0;
+}
